f599856bd70e64f4740844b9e384bd89e022e9ee
Deepak Vyas <deepakvyas@BKGL-IT-MB003.lan>
2022-04-03 22:52:07 +0400

    added value array

--- cypress/integration/cucumber-test/login/list.js
+++ cypress/integration/cucumber-test/login/list.js
@@ -12,11 +12,11 @@ And('List all 5 elements', () => {
 
         // Returns the current element from the loop
         expect(Cypress.$(item)[0].innerText).to.eq(["A/B Testing"]);
-        expect(Cypress.$(item)[0].innerText).to.eq(["Add/Remove Elements"]);
-        expect(Cypress.$(item)[0].innerText).to.eq(["Basic Auth (user and pass: admin)"]);
-        expect(Cypress.$(item)[0].innerText).to.eq(["Broken Images"]);
-        expect(Cypress.$(item)[0].innerText).to.eq(["Challenging DOM"]);
-        expect(Cypress.$(item)[0].innerText).to.eq(["Checkboxes"]);
+        expect(Cypress.$(item)[1].innerText).to.eq(["Add/Remove Elements"]);
+        expect(Cypress.$(item)[2].innerText).to.eq(["Basic Auth (user and pass: admin)"]);
+        expect(Cypress.$(item)[3].innerText).to.eq(["Broken Images"]);
+        expect(Cypress.$(item)[4].innerText).to.eq(["Challenging DOM"]);
+        expect(Cypress.$(item)[5].innerText).to.eq(["Checkboxes"]);
     })
 })
 
